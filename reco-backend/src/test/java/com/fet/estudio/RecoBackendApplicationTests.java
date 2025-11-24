57e140512d05bf1783afe52a02bf027e48f2ecd9
Jose <Jose_ramirezca@fet.edu.co>
2025-11-24 17:40:09 -0500

    Primer Commit

---
+++ reco-backend/src/test/java/com/fet/estudio/RecoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fet.estudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
